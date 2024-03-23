32edc9b0e0e32c178e4afb2ae5772d6d95585c9b
Sachin Kizhakkepurath <sachinkp1997@gmail.com>
2024-03-23 21:14:33 +0530

    added chat feature in user side

--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -5,11 +5,18 @@ import vendorRoute from "../router/vendorRoute";
 import adminRoute from "../router/adminRoute";
 import tokenRoute from "../router/tokenRoute";
 import cors from "cors";
+import { Server } from "socket.io";
 
 console.log(`Inside config/app`);
-const app = express();
 
+const app = express();
 const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +28,17 @@ app.use("/api/vendor", vendorRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/token", tokenRoute);
 
-app.get("/", (req, res) => {
-  res.send("Server Running");
+io.on("connection", (socket: any) => {
+  console.log("A user connected");
+
+  socket.on("message", (data: any) => {
+    console.log(`Received message: ${data}`);
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`A user disconnected`);
+  });
 });
+
 export { httpServer };
